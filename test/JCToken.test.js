5106701a4a773448ca07bdf5c9f46c3d64c62526
Jack Chuma <jackchuma21@gmail.com>
2022-03-17 14:06:40 -0400

    Deployment context added to testing

--- test/JCToken.test.js
+++ test/JCToken.test.js
@@ -11,17 +11,19 @@ describe("JC Token", function() {
     await this.token.deployed();
   });
 
-  it ("should have proper name after deployment", async function () {
-    expect(await this.token.name()).to.equal("JC Token");
-  });
+  context("Deployment", async function() {
+    it ("should have proper name after deployment", async function () {
+      expect(await this.token.name()).to.equal("JC Token");
+    });
 
-  it ("should have proper symbol after deployment", async function() {
-    expect(await this.token.symbol()).to.equal("JCT");
-  });
+    it ("should have proper symbol after deployment", async function() {
+      expect(await this.token.symbol()).to.equal("JCT");
+    });
 
-  it ("deployment should assign total supply of tokens to this.owner", async function() {
-    const ownerBalance = await this.token.balanceOf(this.owner.address);
-    expect(await this.token.totalSupply()).to.equal(ownerBalance);
+    it ("deployment should assign total supply of tokens to this.owner", async function() {
+      const ownerBalance = await this.token.balanceOf(this.owner.address);
+      expect(await this.token.totalSupply()).to.equal(ownerBalance);
+    });
   });
 
   it ("this.owner should be able to mint tokens to someone else", async function() {
@@ -57,51 +59,49 @@ describe("JC Token", function() {
     await expect(this.token.connect(this.alice).transfer(this.bob.address, 50)).to.be.revertedWith("ERC20: transfer amount exceeds balance");
   });
 
-  it ("luckyDouble() can only be called once per year", async function() {
-    const MockJCToken = await ethers.getContractFactory("MockJCToken");
-    const mockToken = await MockJCToken.deploy();
+  context("luckyDouble()", async function() {
+    beforeEach(async function() {
+      const MockJCToken = await ethers.getContractFactory("MockJCToken");
+      this.mockToken = await MockJCToken.deploy();
+    });
 
-    await mockToken.setRandom(500);
-    await mockToken.setBlocksInYear(10);
-    await this.token.mint(this.alice.address, 50);
-    await mockToken.connect(this.alice).luckyDouble();
-    await expect(mockToken.connect(this.alice).luckyDouble()).to.be.revertedWith("Address is locked");
-  });
+    it ("luckyDouble() can only be called once per year", async function() {
+      await this.mockToken.setRandom(500);
+      await this.mockToken.setBlocksInYear(10);
+      await this.token.mint(this.alice.address, 50);
+      await this.mockToken.connect(this.alice).luckyDouble();
+      await expect(this.mockToken.connect(this.alice).luckyDouble()).to.be.revertedWith("Address is locked");
+    });
 
-  it ("check behavior based on random number", async function() {
-    const MockJCToken = await ethers.getContractFactory("MockJCToken");
-    const mockToken = await MockJCToken.deploy();
+    it ("check behavior based on random number", async function() {
+      await this.mockToken.setRandom(901);
+      await this.mockToken.mint(this.alice.address, 50);
+      await this.mockToken.connect(this.alice).luckyDouble();
+      expect(await this.mockToken.balanceOf(this.alice.address)).to.equal(100);
+  
+      await this.mockToken.setRandom(900);
+      await this.mockToken.mint(this.bob.address, 50);
+      await this.mockToken.connect(this.bob).luckyDouble();
+      expect(await this.mockToken.balanceOf(this.bob.address)).to.equal(45);
+    });
 
-    await mockToken.setRandom(901);
-    await mockToken.mint(this.alice.address, 50);
-    await mockToken.connect(this.alice).luckyDouble();
-    expect(await mockToken.balanceOf(this.alice.address)).to.equal(100);
-
-    await mockToken.setRandom(900);
-    await mockToken.mint(this.bob.address, 50);
-    await mockToken.connect(this.bob).luckyDouble();
-    expect(await mockToken.balanceOf(this.bob.address)).to.equal(45);
-  });
-
-  it ("luckyDouble() can be called again after 1 or 2 years", async function() {
-    const MockJCToken = await ethers.getContractFactory("MockJCToken");
-    const mockToken = await MockJCToken.deploy();
-
-    await mockToken.setBlocksInYear(10);
-    await mockToken.setRandom(900);
-    await mockToken.mint(this.alice.address, 50);
-    await mockToken.connect(this.alice).luckyDouble();
-    await expect(mockToken.connect(this.alice).luckyDouble()).to.be.revertedWith("Address is locked");
-
-    await advanceBlockBy(11);
-    expect(await mockToken.connect(this.alice).luckyDouble());
-
-    await mockToken.setRandom(901);
-    await mockToken.mint(this.bob.address, 50);
-    await mockToken.connect(this.bob).luckyDouble();
-    await expect(mockToken.connect(this.bob).luckyDouble()).to.be.revertedWith("Address is locked");
-
-    await advanceBlockBy(21);
-    expect(await mockToken.connect(this.bob).luckyDouble());
+    it ("luckyDouble() can be called again after 1 or 2 years", async function() {
+      await this.mockToken.setBlocksInYear(10);
+      await this.mockToken.setRandom(900);
+      await this.mockToken.mint(this.alice.address, 50);
+      await this.mockToken.connect(this.alice).luckyDouble();
+      await expect(this.mockToken.connect(this.alice).luckyDouble()).to.be.revertedWith("Address is locked");
+  
+      await advanceBlockBy(11);
+      expect(await this.mockToken.connect(this.alice).luckyDouble());
+  
+      await this.mockToken.setRandom(901);
+      await this.mockToken.mint(this.bob.address, 50);
+      await this.mockToken.connect(this.bob).luckyDouble();
+      await expect(this.mockToken.connect(this.bob).luckyDouble()).to.be.revertedWith("Address is locked");
+  
+      await advanceBlockBy(21);
+      expect(await this.mockToken.connect(this.bob).luckyDouble());
+    });
   });
 });
